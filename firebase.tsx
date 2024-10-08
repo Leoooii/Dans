aef183ce0c02d8a95830fc8cd0676f3715d788cd
Leoooii <lioneh39@gmail.com>
2024-10-08 11:12:37 +0300

    pentru Alex

--- firebase.tsx
+++ firebase.tsx
@@ -5,15 +5,7 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDI_IfT85J0B9Fe0VfOtOxYK4_ZI_Htv9s",
-  authDomain: "dansschedule-fd2f8.firebaseapp.com",
-  projectId: "dansschedule-fd2f8",
-  storageBucket: "dansschedule-fd2f8.appspot.com",
-  messagingSenderId: "102074292781",
-  appId: "1:102074292781:web:83381111dbe9b605a376a4",
-  measurementId: "G-9R1D3Y1PY3",
-};
+const firebaseConfig = {};
 
 // Check if any Firebase apps are already initialized, if not, initialize one
 let app;
